ac08de8532a49a56946a172994d5a6b07a018447
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-05-15 15:07:07 +0300

    instrumentation

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 )
 
 func main() {
+	ctx := context.Background()
+	shutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer shutdown(ctx)
+
 	http.HandleFunc("/rolldice", rolldice)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
